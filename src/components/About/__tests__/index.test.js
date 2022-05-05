39c6d18ffa73bdac3f64d5e00035a147db042f98
jrwj97 <jrwj97@aol.com>
2022-05-05 16:39:48 -0400

    added application state for each category

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,18 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import About from "..";
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
-const { asFragment } = render(<About />);
 
-describe("About component", () => {
-  //first test
-  it("renders", () => {
+describe('About component renders', () => {
+  it('renders', () => {
     render(<About />);
-  })
-  //second test
-  it("matches snapshot DOM node structure", () => {
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
-  })
+  });
 })
